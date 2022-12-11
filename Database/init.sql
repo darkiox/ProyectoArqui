2fa48086a8c8e374d98ebb811c5f49e6b6079b98
Javier Guerrero <82735257+JavierGv1@users.noreply.github.com>
2022-12-11 20:41:13 -0300

    Update init.sql

--- Database/init.sql
+++ Database/init.sql
@@ -4,4 +4,22 @@ CREATE TABLE IF NOT EXISTS sales(id INT, sku INT, cantidad INT, valor INT, valor
 CREATE TABLE IF NOT EXISTS purchases(id INT, sku INT, cantidad INT, valor INT, valortotal INT, fecha date);
 INSERT INTO users VALUES ('admin', 'admin', 'admin@admin.com', 'adminpassword', 0);
 INSERT INTO products(nombre,categoria,preciocompra,precioventa,stock) VALUES ('Chocolate','Comida', 1000, 1500, 30);
-INSERT INTO products(nombre,categoria,preciocompra,precioventa,stock) VALUES ('PlayStation','Electrodomesticos', 400000, 450000, 5);
+INSERT INTO products(nombre,categoria,preciocompra,precioventa,stock) VALUES ('Doritos','Comida', 1000, 2000, 20);
+INSERT INTO products(nombre,categoria,preciocompra,precioventa,stock) VALUES ('Papa Frita','Comida', 800, 1200, 30);
+INSERT INTO products(nombre,categoria,preciocompra,precioventa,stock) VALUES ('Masticables','Comida', 100, 200, 100);
+INSERT INTO products(nombre,categoria,preciocompra,precioventa,stock) VALUES ('PlayStation 5','Electrodomesticos', 400000, 450000, 5);
+INSERT INTO products(nombre,categoria,preciocompra,precioventa,stock) VALUES ('Xbox Serie X','Electrodomesticos', 400000, 450000, 10);
+INSERT INTO products(nombre,categoria,preciocompra,precioventa,stock) VALUES ('Microondas','Electrodomesticos', 100000, 150000, 2);
+INSERT INTO products(nombre,categoria,preciocompra,precioventa,stock) VALUES ('Zapatilla Roja','Calzado', 30000, 45000, 5);
+INSERT INTO products(nombre,categoria,preciocompra,precioventa,stock) VALUES ('Zapato Formal','Calzado', 35000, 50000, 8);
+INSERT INTO products(nombre,categoria,preciocompra,precioventa,stock) VALUES ('Camiseta Polo','Vestuario', 25000, 450000, 5);
+INSERT INTO sales(id, sku, cantidad , valor, valortotal, fecha) VALUES (1, 1, 10, 10000, 100000,'2022-12-11');
+INSERT INTO sales(id, sku, cantidad , valor, valortotal, fecha) VALUES (2, 2, 20, 1000, 200000,'2022-11-8');); 
+INSERT INTO sales(id, sku, cantidad , valor, valortotal, fecha) VALUES (3, 5, 30, 800, 24000,'2022-8-10'););
+INSERT INTO sales(id, sku, cantidad , valor, valortotal, fecha) VALUES (4, 4, 100, 200, 20000,'2022-10-18'););
+INSERT INTO sales(id, sku, cantidad , valor, valortotal, fecha) VALUES (5, 5, 90, 600, 54000,'2022-12-8'););
+INSERT INTO sales(id, sku, cantidad , valor, valortotal, fecha) VALUES (6, 6, 70, 500, 35000,'2022-12-3'););
+INSERT INTO sales(id, sku, cantidad , valor, valortotal, fecha) VALUES (7, 8, 9, 300, 2700,'2022-11-30'););
+INSERT INTO sales(id, sku, cantidad , valor, valortotal, fecha) VALUES (8, 9, 10, 150000, 1500000,'2022-2-25'););
+INSERT INTO sales(id, sku, cantidad , valor, valortotal, fecha) VALUES (9, 1, 12, 50000, 600000,'2022-10-8'););
+
